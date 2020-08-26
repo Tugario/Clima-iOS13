946b7fa4eacb52c651825ed07bbde86ce41c41b0
Tiger <56575129+Tugario@users.noreply.github.com>
2020-08-26 13:55:07 -0700

    Decode information from JSON
    Also set up done for UI updates

---
+++ Clima-iOS13/Clima/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Tiger Chiang on 8/25/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let id: Int
+}
--- Clima-iOS13/Clima/Model/WeatherManager.swift
+++ Clima-iOS13/Clima/Model/WeatherManager.swift
@@ -8,12 +8,65 @@
 
 import Foundation
 
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(weather: WeatherModel)
+}
+
 struct WeatherManager {
-    let weatherUrl = "http://api.openweathermap.org/data/2.5/weather?appid=f1f244135e45ff3e1651760e1387f801&units=imperial"
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=f1f244135e45ff3e1651760e1387f801&units=imperial"
+    
+    var delegate: WeatherManagerDelegate?
     
     func fetchWeather(cityName: String) {
-        let urlString = "\(weatherUrl)&q=(cityName)"
-        print(urlString)
+        let urlString = "\(weatherUrl)&q=\(cityName)"
+        performRequest(urlString: urlString)
     }
+    
+    func performRequest(urlString: String) {
+        // Create URL
+        if let url = URL(string: urlString) {
+            
+            // Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            // Give the session a task
+            let task = session.dataTask(with: url) {(data, response, error) in
+                if(error != nil) {
+                    print(error!)
+                    return
+                }
+                if let safeData = data {
+                    // we need to add self here because we are in a closure
+                    // and functions within a closure in the class need the self
+                    // to properly define where and what its trying to call
+                    if let weather = self.parseJSON(weatherData: safeData) {
+                        self.delegate?.didUpdateWeather(weather: weather)
+                    }
+                }
+            }
+            
+            // Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) -> WeatherModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+
+            return weather
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+
 }
 
