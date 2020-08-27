5ad407ee73d84fbb030f687ddacb4d379f20a3e4
Tiger <56575129+Tugario@users.noreply.github.com>
2020-08-27 15:17:02 -0700

    Added Current Location functionality
    Auto collects information upon approval click at the start.
    Implemented the current location button.
    Updates UI when searching with a symbol, temperature, and location

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,79 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Tiger Chiang on 8/21/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
+
+struct WeatherManager {
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=f1f244135e45ff3e1651760e1387f801&units=imperial"
+    
+    var delegate: WeatherManagerDelegate?
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherUrl)&q=\(cityName)"
+        performRequest(urlString)
+    }
+    
+    func fetchWeather(_ latitude: CLLocationDegrees, _ longitude: CLLocationDegrees) {
+        let urlString = "\(weatherUrl)&lat=\(latitude)&lon=\(longitude)"
+        performRequest(urlString)
+    }
+    
+    func performRequest(_ urlString: String) {
+        // Create URL
+        if let url = URL(string: urlString) {
+            
+            // Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            // Give the session a task
+            let task = session.dataTask(with: url) {(data, response, error) in
+                if(error != nil) {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data {
+                    // we need to add self here because we are in a closure
+                    // and functions within a closure in the class need the self
+                    // to properly define where and what its trying to call
+                    if let weather = self.parseJSON(safeData) {
+                        self.delegate?.didUpdateWeather(self, weather: weather)
+                    }
+                }
+            }
+            
+            // Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ weatherData: Data) -> WeatherModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+
+            return weather
+        } catch {
+            self.delegate?.didFailWithError(error: error)
+            return nil
+        }
+    }
+    
+    
+}
+
